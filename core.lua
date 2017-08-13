0725eb1e6b7f032f560377e5cee3644048ab8451
Rainrider <rainrider.wow@gmail.com>
2017-08-13 02:38:48 +0200

    Initial commit

---
+++ core.lua
@@ -0,0 +1,108 @@
+local addonName, addon = ...
+local coloredAddonName = _G.format("|cff0099CC%s|r", addonName)
+
+local db
+
+local date = _G.date
+local print = _G.print
+local select = _G.select
+
+local mt = {
+	__call = function(t, self, ...)
+		for i = 1, #t do
+			t[i](self, ...)
+		end
+	end
+}
+
+local AceConfig = _G.LibStub('AceConfig-3.0')
+local AceConfigDialog = _G.LibStub('AceConfigDialog-3.0')
+
+local frame = _G.CreateFrame('Frame', 'etest')
+addon.frame = frame
+
+function frame:AddEvent(data)
+	local event = data.event
+	if not event then return end
+
+	local handler = data.handler
+	if handler and type(handler) ~= 'table' then
+		handler = { handler }
+	end
+
+	if handler then
+		local callbacks = {}
+		for _, callback in ipairs(handler) do
+			if type(callback) == 'string' and callback ~= '' then
+				local func, err = loadstring(callback) -- TODO: second param to loadstring
+				if func then
+					callbacks[#callbacks + 1] = func
+				else
+					print(err) -- TODO: error here?
+				end
+			elseif type(callback) == 'function' then
+				callbacks[#callbacks + 1] = callback
+			end
+		end
+
+		local current = self[event]
+		if current then
+			for i = 1, #callbacks do
+				current[#current + 1] = callbacks[i]
+			end
+		else
+			self[event] = setmetatable(callbacks, mt)
+		end
+	end
+
+	if data.unit1 or data.unit2 then
+		self:RegisterUnitEvent(event, data.unit1, data.unit2)
+	else
+		self:RegisterEvent(event)
+	end
+end
+
+function frame:RemoveEvent(data)
+	local event = data.event
+	if not event then return end
+
+	self:UnregisterEvent(event)
+	self[event] = nil
+end
+
+function frame.PrintArgs(_, event, ...)
+	print(date('%X'), coloredAddonName, event)
+	for i = 1, select('#', ...) do
+		local arg = select(i, ...)
+		print(i, ':', arg)
+	end
+end
+
+frame:SetScript('OnEvent', function(self, event, ...)
+	if not self[event] then
+		self:PrintArgs(event, ...)
+	else
+		self[event](self, db, event, ...)
+	end
+end)
+frame:RegisterEvent('ADDON_LOADED')
+
+function frame:ADDON_LOADED(_, _, name)
+	if name ~= addonName then return end
+
+	_G.HandleItDB = _G.HandleItDB or {}
+	db = _G.HandleItDB
+	db.events = db.events or {}
+	addon.db = db
+
+	for _, data in next, db.events do
+		if data.enabled then
+			self:AddEvent(data)
+		end
+	end
+
+	AceConfig:RegisterOptionsTable(addonName, addon.GetOptions(), '/handleit')
+	AceConfigDialog:AddToBlizOptions(addonName)
+
+	self:UnregisterEvent('ADDON_LOADED')
+end
---
+++ options.lua
@@ -0,0 +1,161 @@
+local addonName, addon = ...
+
+local SAMPLE_HANDLER = [[
+local self, db, event = ...
+self:PrintArgs(event, select(4, ...))
+]]
+
+function addon.GetOptions()
+	local db = addon.db
+	local event = {
+		selected = next(db.events)
+	}
+
+	function event:Select(key)
+		self.selected = key
+	end
+
+	function event:GetSelected()
+		return self.selected
+	end
+
+	function event:HasNoSelection()
+		return self.selected == nil
+	end
+
+	function event:Create()
+		local key = #db.events + 1
+		db.events[key] = {}
+		db.events[key].event = 'New event #' .. key
+		db.events[key].enabled = false
+		db.events[key].handler = SAMPLE_HANDLER
+		self:Select(key)
+	end
+
+	function event:Delete()
+		addon.frame:RemoveEvent(db.events[self.selected])
+		table.remove(db.events, self.selected)
+		self.selected = next(db.events)
+	end
+
+	function event:Get(property)
+		local data = db.events[self.selected]
+		if not data then return end
+		return data[property]
+	end
+
+	function event:Set(property, value)
+		local data = db.events[self.selected]
+		if not data or data[property] == value then return end
+
+		if property ~= 'enabled' then
+			data.enabled = false
+		else
+			if value then
+				addon.frame:AddEvent(data)
+			else
+				addon.frame:RemoveEvent(data)
+			end
+		end
+		data[property] = value
+	end
+
+	function event:HasNoEvents()
+		return not next(db.events)
+	end
+
+	local eventNames = {}
+	function event:GetEventsList()
+		wipe(eventNames)
+		for i = 1, #db.events do
+			local name = db.events[i].event
+			eventNames[i] = name
+		end
+
+		return eventNames
+	end
+
+	return {
+		name = 'Events',
+		type = 'group',
+		handler = event,
+		args = {
+			selectedEvent = {
+				name = 'Selected Event',
+				type = 'select',
+				order = 10,
+				get = 'GetSelected',
+				set = function(_, key) event:Select(key) end,
+				values = 'GetEventsList',
+				hidden = 'HasNoEvents',
+			},
+			newEvent = {
+				name = 'New event',
+				type = 'execute',
+				order = 20,
+				func = 'Create',
+			},
+			edit = {
+				name = 'Edit event',
+				type = 'group',
+				order = 30,
+				inline = true,
+				get = function(info) return event:Get(info[#info]) end,
+				set = function(info, value) event:Set(info[#info], value) end,
+				hidden = 'HasNoSelection',
+				args = {
+					enabled = {
+						name = 'Enabled',
+						type = 'toggle',
+						order = 10,
+					},
+					delete = {
+						name = 'Delete',
+						type = 'execute',
+						order = 15,
+						func = 'Delete',
+						confirm = true,
+						confirmText = 'Permanently delete this event?',
+					},
+					event = {
+						name = 'Event name',
+						type = 'input',
+						order = 20,
+						width = 'full',
+						validate = function(_, value)
+							if not value or value == "" then
+								return 'You must enter an event name.'
+							end
+							for i = 1, #eventNames do
+								if eventNames[i] == value then
+									return 'An event with that name already exists.'
+								end
+							end
+
+							return true
+						end,
+					},
+					unit1 = {
+						name = 'First unit',
+						type = 'input',
+						order = 30,
+					},
+					unit2 = {
+						name = 'Second unit',
+						type = 'input',
+						order = 35,
+					},
+					handler = {
+						name = 'Event handler',
+						type = 'input',
+						order = 40,
+						multiline = 15,
+						width = 'full',
+					},
+				},
+			},
+		},
+	}
+end
+
+
